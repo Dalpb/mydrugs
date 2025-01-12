4a59fe52c599ae6dffff0396c4adf7eeb7e27593
Fidnix <fidel.moises0@gmail.com>
2025-01-12 17:15:13 -0500

    Register view

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import BasicForm from "@/components/BasicForm.vue";
+import { RouterLink } from 'vue-router';
+</script>
+<template>
+    <main>
+        <BasicForm title="Registrar" :inputs="[
+            { 'label': 'Nombre de usuario', 'type': 'text', 'name': 'username' },
+            { 'label': 'Correo electrónico', 'type': 'email', 'name': 'email' },
+            { 'label': 'Contraseña', 'type': 'password', 'name': 'password' },
+            { 'label': 'Confirmar contraseña', 'type': 'password', 'name': 'confirm-password' },]">
+            <RouterLink to="/login">¿Ya tienes una cuenta? Inicie sesión aquí</RouterLink>
+        </BasicForm>
+    </main>
+</template>
+
+<style scoped>
+main {
+    display: flex;
+    justify-content: center;
+
+    padding-top: 4rem;
+}
+</style>
