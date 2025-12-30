6be86625da583ec82836124da47b67dcbdc2ebc3
Dalpb <dpalomino032002@gmail.com>
2025-12-30 15:58:19 -0500

    feature: update router main section

--- src/components/DrugSection.vue
+++ src/components/DrugSection.vue
@@ -1,163 +1,188 @@
 <script lang="ts" setup>
-import "../styles/myanimations.css"
-import RangeStar from './RangeStar.vue';
-import { changeFirstChildColor } from '@/utils/colorHandlers';
-import type { Drug } from '@/interfaces/models/Drug.interface';
+import "../styles/myanimations.css";
+import RangeStar from "./RangeStar.vue";
+import { changeFirstChildColor } from "@/utils/colorHandlers";
+import type { Drug } from "@/interfaces/models/Drug.interface";
 import plusIcon from "./plusIcon.vue";
-interface Props{
-    drugInfo:Drug,
-    changing:boolean
+import { useRouter } from "vue-router";
+interface Props {
+  drugInfo: Drug;
+  changing: boolean;
 }
-const {drugInfo,changing} = defineProps<Props>();
-const {name,description,recomendation,image,priceBTC,priceETH,rating,drugColor} = drugInfo;
-
+const { drugInfo, changing } = defineProps<Props>();
+const router = useRouter();
+const {
+  name,
+  description,
+  recomendation,
+  image,
+  priceBTC,
+  priceETH,
+  rating,
+  drugColor,
+  id,
+} = drugInfo;
+console.log(id);
+const goShopDrug = () => {
+  router.push({ name: "Products", params: { id: id } });
+};
 </script>
 <template>
-    <section class="main-section">
-        <article class="inf-1 "  v-bind:class="changing ? 'hide-animation' : 'show-animation' ">
-            <p>{{ name }}</p>
-            <p class="a">{{ description }}</p>
-            <range-star :rating="rating"/>
-        </article>
-        <picture class="main-picture" v-bind:class="changing && 'clean-animation'">
-            <img :src="image" :alt="image" />
-        </picture>
-        <article class="inf-2" v-bind:class="changing ? 'hide-animation' : 'show-animation'" >
-            <p>{{ priceBTC }}BTC / {{ priceETH }}ETH</p>
-            <p>{{ recomendation }}</p>
-            <div 
-            class="square"
-            @mouseover="(e)=>changeFirstChildColor(e,drugColor!)"
-            @mouseleave="(e)=>changeFirstChildColor(e,'white')">
-                <plus-icon id="plus" class="plus"/>
-            </div>
-        </article>
-    </section>
+  <section class="main-section">
+    <article
+      class="inf-1"
+      v-bind:class="changing ? 'hide-animation' : 'show-animation'"
+    >
+      <p>{{ name }}</p>
+      <p class="a">{{ description }}</p>
+      <range-star :rating="rating" />
+    </article>
+    <picture class="main-picture" v-bind:class="changing && 'clean-animation'">
+      <img :src="image" :alt="image" />
+    </picture>
+    <article
+      class="inf-2"
+      v-bind:class="changing ? 'hide-animation' : 'show-animation'"
+    >
+      <p>{{ priceBTC }}BTC / {{ priceETH }}ETH</p>
+      <p>{{ recomendation }}</p>
+      <div
+        class="square"
+        @mouseover="(e)=>changeFirstChildColor(e,drugColor!)"
+        @mouseleave="(e) => changeFirstChildColor(e, 'white')"
+        @click="goShopDrug"
+      >
+        <plus-icon id="plus" class="plus" />
+      </div>
+    </article>
+  </section>
 </template>
 <style lang="css" scoped>
-.a{
-    color:#ffffffb3;
+.a {
+  color: #ffffffb3;
 }
 /* content styles */
-.show-animation{
-    animation-duration: .45s;
+.show-animation {
+  animation-duration: 0.45s;
 }
-.main-section{
-    width: 100%;
-    height: 97%;
-    align-self: flex-start;
-    display: grid;
-    grid-template-columns:  minmax(100px,.8fr) minmax(100px,1fr) minmax(100px,.8fr);
-    grid-template-rows: repeat(8,1fr);
-    column-gap: 1rem;
+.main-section {
+  width: 100%;
+  height: 97%;
+  align-self: flex-start;
+  display: grid;
+  grid-template-columns: minmax(100px, 0.8fr) minmax(100px, 1fr) minmax(
+      100px,
+      0.8fr
+    );
+  grid-template-rows: repeat(8, 1fr);
+  column-gap: 1rem;
 }
-.main-picture{
-    grid-column: 2/3;
-    grid-row: 1/9;
-    place-self: center;
-    width: 100%;
+.main-picture {
+  grid-column: 2/3;
+  grid-row: 1/9;
+  place-self: center;
+  width: 100%;
 }
-.main-picture img{
-    width: 100%;
-    object-fit: contain;
+.main-picture img {
+  width: 100%;
+  object-fit: contain;
 }
 
-.main-section article:first-of-type{
-    grid-row: 2/5;
-    text-align: end;
-    white-space: pre;
-    text-transform: uppercase;
+.main-section article:first-of-type {
+  grid-row: 2/5;
+  text-align: end;
+  white-space: pre;
+  text-transform: uppercase;
 }
-.main-section article:last-of-type{
-    grid-column: 3/4;
-    grid-row: 5/8;
+.main-section article:last-of-type {
+  grid-column: 3/4;
+  grid-row: 5/8;
 }
 /* end content styles */
 
 /* letter styles */
-.inf-1 p{
-    font-size: 2.5rem;
- }
-.inf-1 p:first-child{
-    font-weight: 900;
-    font-size: 3.5rem;
-    text-shadow: 0px 0px 15px  rgba(0, 0, 0, 0.347);
+.inf-1 p {
+  font-size: 2.5rem;
 }
-.inf-2{
-    display: flex;
-    flex-direction: column;
-    gap:1rem;
-    align-items: start;
+.inf-1 p:first-child {
+  font-weight: 900;
+  font-size: 3.5rem;
+  text-shadow: 0px 0px 15px rgba(0, 0, 0, 0.347);
 }
-.inf-2 p{
-    font-size: 1.25rem;
+.inf-2 {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: start;
 }
-.inf-2 p:first-child{
-    font-weight: 400;
-    font-size: 2rem;
+.inf-2 p {
+  font-size: 1.25rem;
+}
+.inf-2 p:first-child {
+  font-weight: 400;
+  font-size: 2rem;
 }
 /* end letter styles */
 
-
 /* Decoration for the square */
-.square{
-    width: 3.6rem;
-    aspect-ratio: 1;
-    display: flex;
-    justify-content: center;
-    border: 2px solid white;
-    cursor: pointer;
+.square {
+  width: 3.6rem;
+  aspect-ratio: 1;
+  display: flex;
+  justify-content: center;
+  border: 2px solid white;
+  cursor: pointer;
 }
-.square:hover{
-    background-color: rgba(255, 255, 255, 0.164);
+.square:hover {
+  background-color: rgba(255, 255, 255, 0.164);
 }
-.square:hover .plus{
-    color:transparent;
+.square:hover .plus {
+  color: transparent;
 }
-.plus{
-    width: 2rem;
-    height: 100%;
+.plus {
+  width: 2rem;
+  height: 100%;
 }
 /* end of decoration */
 
 /* responsive desgin */
 @media screen and (max-width: 1100px) {
-    .main-section{
-        display: grid;
-        grid-template-columns: 1fr;
-        grid-template-rows: 1fr 1fr ;
-        row-gap: 1rem;
-        column-gap: 0rem;
-    }
-    .main-picture{
-        grid-row: 2/5;
-        grid-column: 1/2;
-        align-self: flex-start;
-        width: 85%;
-    }
-    .main-section article:first-of-type{
-        grid-row: 1/2;
-        grid-column: 1/2;
-        align-self: end;
-        text-align: center;
-    }
-    .main-section article:last-of-type{
+  .main-section {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr;
+    row-gap: 1rem;
+    column-gap: 0rem;
+  }
+  .main-picture {
+    grid-row: 2/5;
+    grid-column: 1/2;
+    align-self: flex-start;
+    width: 85%;
+  }
+  .main-section article:first-of-type {
+    grid-row: 1/2;
+    grid-column: 1/2;
+    align-self: end;
+    text-align: center;
+  }
+  .main-section article:last-of-type {
     display: none;
-    }
+  }
 
-    .inf-1 p:first-child{
-        font-size: 2.8rem;
-    }
-    .inf-1 p{
+  .inf-1 p:first-child {
+    font-size: 2.8rem;
+  }
+  .inf-1 p {
     font-size: 1.8rem;
-    }
+  }
 }
-@media screen and (max-height: 600px){
-    .inf-1 p:first-child{
-        font-size: 1.8rem;
-    }
-    .inf-1 p{
+@media screen and (max-height: 600px) {
+  .inf-1 p:first-child {
+    font-size: 1.8rem;
+  }
+  .inf-1 p {
     font-size: 1rem;
-    }
+  }
 }
 </style>
--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,89 +1,133 @@
 <script lang="ts" setup>
-import "../styles/myanimations.css"
-interface Props{
-    img?:string,
+import "../styles/myanimations.css";
+import { useRouter } from "vue-router";
+interface Props {
+  img?: string;
 }
-const {img} = defineProps<Props>();
+const router = useRouter();
+const { img } = defineProps<Props>();
+const goToShop = () => router.push({ name: "Shop" });
+
+const goToSafety = () => router.push({ name: "Safety" });
+
+const goToFaq = () => router.push({ name: "FAQ" });
+
+const goToContact = () => router.push({ name: "Contact" });
 </script>
 
 <template>
-    <section class="main-section">
-        <article class="opt shp">
-            <p class="show-animation dly-1">SHOP</p>
-            <p class="show-animation dly-2">ALL THE DRUGS<br>YOU WANT</p>
-        </article>
-        <article class="opt sft">
-            <p class="show-animation dly-3">SAFETY</p>
-            <p class="show-animation dly4">IMPORTANT <br>INFORMATION</p>
-        </article>
-        <picture class="main-picture ">
-            <img :src="img" alt="home-drug"/>
-        </picture>
-        <article class="opt faq">
-            <p class="show-animation dly-5">FAQ</p>
-            <p class="show-animation dly-6">ANSWERING <br>YOUR QUESTIONS </p>
-        </article>
-        <article class="opt ctc">
-            <p class="show-animation dly-7">CONTACT</p>
-            <p class="show-animation dly-8">SUGGESTIONS AND<br>FEEDBACK</p>               
-        </article>
-    </section>
+  <section class="main-section">
+    <article class="opt shp bright" @click="goToShop">
+      <p class="show-animation dly-1">SHOP</p>
+      <p class="show-animation dly-2">ALL THE DRUGS<br />YOU WANT</p>
+    </article>
+    <article class="opt sft bright" @click="goToSafety">
+      <p class="show-animation dly-3">SAFETY</p>
+      <p class="show-animation dly4">IMPORTANT <br />INFORMATION</p>
+    </article>
+    <picture class="main-picture">
+      <img :src="img" alt="home-drug" />
+    </picture>
+    <article class="opt faq bright">
+      <p class="show-animation dly-5" @click="goToFaq">FAQ</p>
+      <p class="show-animation dly-6">ANSWERING <br />YOUR QUESTIONS</p>
+    </article>
+    <article class="opt ctc bright">
+      <p class="show-animation dly-7" @click="goToContact">CONTACT</p>
+      <p class="show-animation dly-8">SUGGESTIONS AND<br />FEEDBACK</p>
+    </article>
+  </section>
 </template>
 <style lang="css" scoped>
-.main-section{
-    width: 100%;
-    height: 97%;
-    align-self: flex-start;
-    display: grid;
-    grid-template-columns:  minmax(100px,.8fr) minmax(500px,1fr) minmax(100px,.8fr);
-    grid-template-rows: repeat(6,1fr);
-    column-gap: 1rem;
+.main-section {
+  width: 100%;
+  height: 97%;
+  align-self: flex-start;
+  display: grid;
+  grid-template-columns: minmax(100px, 0.8fr) minmax(500px, 1fr) minmax(
+      100px,
+      0.8fr
+    );
+  grid-template-rows: repeat(6, 1fr);
+  column-gap: 1rem;
 }
-.main-picture{
-    grid-column: 2/3;
-    grid-row: 1/7;
-    place-content: center;
-    width: 97.5%;
+.main-picture {
+  grid-column: 2/3;
+  grid-row: 1/7;
+  place-content: center;
+  width: 97.5%;
 }
-.main-picture img{
-    width: 100%;
-    object-fit: cover;
+.main-picture img {
+  width: 100%;
+  object-fit: cover;
 }
-.opt p{
-    font-size: 2rem;
- }
-.opt p:first-child{
-    font-weight: 900;
-    font-size: 3.3rem;
+.opt {
+  width: fit-content;
 }
-.opt p:last-child{
-    color:#ffffffb3;
+.opt p {
+  font-size: 2rem;
+}
+.opt p:first-child {
+  font-weight: 900;
+  font-size: 3.3rem;
+}
+.opt p:last-child {
+  color: #ffffffb3;
 }
 
-.shp,.faq{
-grid-row: 2/3;
+.shp,
+.faq {
+  grid-row: 2/3;
 }
-.ctc,.sft{
-    grid-row: 5/6;
+.shp,
+.sft {
+  justify-self: end;
 }
-.shp,.sft{
-    text-align: end;
+.faq,
+.ctc {
+  justify-self: start;
+}
+.ctc,
+.sft {
+  grid-row: 5/6;
+}
+.shp,
+.sft {
+  text-align: end;
+}
+.bright {
+  transition: all 0.4s cubic-bezier(0.25, 0.8, 0.25, 1);
+  padding: 0.5rem;
+}
+
+/* Estado HOVER */
+.bright:hover {
+  transform: translateY(-8px) scale(1.02);
+  cursor: pointer;
+}
+
+.bright:hover p {
+  text-shadow: 0 0 12px rgba(255, 255, 255, 0.8),
+    0 0 20px rgba(255, 255, 255, 0.4);
+}
+
+.bright:hover p:last-child {
+  color: #ffffff;
 }
 @media screen and (max-width: 1100px) {
-    .main-section{
-        grid-template-columns: 1fr;
-        grid-template-rows: 1fr;
-    }
-    .main-picture{
-        grid-column: 1/2;
-        grid-row: 1/2;
-        align-self: center;
-        justify-self: center;
-        width: 150%;
-
-    }
-    .opt{
-       display: none; 
-    }
+  .main-section {
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr;
+  }
+  .main-picture {
+    grid-column: 1/2;
+    grid-row: 1/2;
+    align-self: center;
+    justify-self: center;
+    width: 150%;
+  }
+  .opt {
+    display: none;
+  }
 }
 </style>
